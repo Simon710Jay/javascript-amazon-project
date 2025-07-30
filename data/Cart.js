936a5e39425905c2b898c47e967d76b2eb9fbab0
SimonJay <simonjay710@gmail.com>
2025-07-30 11:29:35 +0100

    Save cart in localStorage

--- data/Cart.js
+++ data/Cart.js
@@ -13,7 +13,7 @@ if (!cart) {
 }
 
 function saveToStorage() {
-    localStorage.setItem('cart',    JSON.stringify(cart));
+    localStorage.setItem('cart', JSON.stringify(cart));
 }
 
 export function addToCart(productId) {
